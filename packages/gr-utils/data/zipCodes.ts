1773830342ceb65dd0fbe90c49695f7506c9d2ba
John Tsevdos <tsevdosjohn@gmail.com>
2022-10-25 11:04:12 +0300

    zipCodes

--- packages/gr-utils/data/zipCodes.ts
+++ packages/gr-utils/data/zipCodes.ts
@@ -1,10 +1,44 @@
 export const zipCodes = {
   el: [
     {
-      zipCode: 30015,
-      prefecture: "Αιτωλοακαρνανίας",
+      zipCodes: [30015],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
       area: "Άγιος Ανδρέας Γαβαλού",
-      address: "Άγιος Ανδρέας Γαβαλού",
+    },
+    {
+      zipCodes: [30021],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
+      area: "Άγιος Βλάσιος",
+    },
+    {
+      zipCodes: [30027],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
+      area: "Άγιος Κωνσταντίνος",
+    },
+    {
+      zipCodes: [30131, 30132, 30133, 30150],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
+      area: "Αγρίνιο",
+    },
+    {
+      zipCodes: [30400],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
+      area: "Αιτωλικό",
+    },
+    {
+      zipCodes: [30002],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
+      area: "Άκτιο",
+    },
+    {
+      zipCodes: [30500],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
+      area: "Αμφιλοχία",
+    },
+    {
+      zipCodes: [30020],
+      prefecture: "ΑΙΤΩΛΟΑΚΑΡΝΑΝΙΑΣ",
+      area: "Αντίρριο",
     },
   ],
 } as const;
