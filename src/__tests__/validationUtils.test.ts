a060888efa6a10b7c88bf58b25ed6fbd88a3eeae
vorillaz <theodorevorillas@gmail.com>
2024-02-23 14:04:04 +0100

    chore(validationutils): remove empty line

--- src/__tests__/validationUtils.test.ts
+++ src/__tests__/validationUtils.test.ts
@@ -1,4 +1,4 @@
-import { validatePostalCode } from "../validationUtils";
+import { validatePostalCode, validateAFM } from "../validationUtils";
 
 describe("validatePostalCode", () => {
   it("returns true on existing postal codes", () => {
@@ -18,5 +18,21 @@ describe("validatePostalCode", () => {
     expect(validatePostalCode("99999")).toBe(false);
     expect(validatePostalCode("98765")).toBe(false);
     expect(validatePostalCode("56789")).toBe(false);
+  });
+});
+
+describe("validateAFM", () => {
+  it("returns false for invalid afms", () => {
+    expect(validateAFM("12345678")).toBe(false);
+    expect(validateAFM("xxxxxxxx")).toBe(false);
+    expect(validateAFM("141212176")).toBe(false);
+    expect(validateAFM("111111111")).toBe(false);
+  });
+  it("returns false for valid afms", () => {
+    expect(validateAFM("011111111")).toBe(true);
+    expect(validateAFM("150892297")).toBe(true);
+    expect(validateAFM("126668921")).toBe(true);
+    expect(validateAFM("234893562")).toBe(true);
+    expect(validateAFM("126668921")).toBe(true);
   });
 });
--- src/validationUtils.ts
+++ src/validationUtils.ts
@@ -2,6 +2,21 @@ import postalCodes from "../data/postal-codes.json";
 
 export function validatePostalCode(postalCode: string): boolean {
   const validPostalCodes = postalCodes.flatMap(({ postalCodes }) => [...postalCodes]);
-
   return validPostalCodes.includes(postalCode);
+}
+
+export function validateAFM(afm: string | string): boolean {
+  const strAfm = afm.toString();
+  if (!strAfm.match(/^\d{9}$/) || strAfm == "000000000") {
+    return false;
+  }
+
+  const last = parseInt(strAfm.charAt(8));
+  let sum = 0,
+    power = 1;
+  for (let i = 7; i >= 0; i--) {
+    power *= 2;
+    sum += parseInt(strAfm.charAt(i)) * power;
+  }
+  return sum % 11 === last;
 }
