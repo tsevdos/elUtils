1afb818e0adebf9efe185b10757d10bd18c4483d
Aris Giotis <apjoti@gmail.com>
2024-10-27 13:47:44 +0200

    feat(validationUtils): add more test cases

--- src/__tests__/validationUtils.test.ts
+++ src/__tests__/validationUtils.test.ts
@@ -59,6 +59,8 @@ describe("validateVATNumber", () => {
     expect(validateVATNumber("xxxxxxxx")).toBe(false);
     expect(validateVATNumber("141212176")).toBe(false);
     expect(validateVATNumber("111111111")).toBe(false);
+    expect(validateVATNumber("311199349")).toBe(false);
+    expect(validateVATNumber("000000000")).toBe(false);
   });
   it("returns true for valid afms", () => {
     expect(validateVATNumber("011111111")).toBe(true);
@@ -66,6 +68,54 @@ describe("validateVATNumber", () => {
     expect(validateVATNumber("126668921")).toBe(true);
     expect(validateVATNumber("234893562")).toBe(true);
     expect(validateVATNumber("126668921")).toBe(true);
+    expect(validateVATNumber("565830300")).toBe(true);
+  });
+
+  it("should work with input older than year 1999", () => {
+    expect(validateVATNumber("050503557")).toBe(true);
+    expect(validateVATNumber("056203761")).toBe(false);
+  });
+
+  it("should work with legal entities, i.e. starting with 7-9", () => {
+    expect(validateVATNumber("737616950")).toBe(true);
+    expect(validateVATNumber("816267772")).toBe(true);
+    expect(validateVATNumber("985117351")).toBe(true);
+  });
+
+  it("should work with physical entities, i.e. starting with 1-4", () => {
+    expect(validateVATNumber("115410916")).toBe(true);
+    expect(validateVATNumber("234383562")).toBe(true);
+    expect(validateVATNumber("302054370")).toBe(true);
+    expect(validateVATNumber("479901058")).toBe(true);
+  });
+
+  it("should work with specific repeated digits tolerance", () => {
+    expect(validateVATNumber("267687344")).toBe(false);
+    expect(validateVATNumber("830777267")).toBe(false);
+  });
+
+  it("should not accept more than 9 digits", () => {
+    expect(validateVATNumber("23798790484567")).toBe(false);
+    expect(validateVATNumber("237987904860")).toBe(false);
+    expect(validateVATNumber("23798790489")).toBe(false);
+    expect(validateVATNumber("2379879048")).toBe(false);
+  });
+
+  it("should not accept less than 9 digits", () => {
+    expect(validateVATNumber("42496046")).toBe(false);
+    expect(validateVATNumber("4249604")).toBe(false);
+    expect(validateVATNumber("424960")).toBe(false);
+    expect(validateVATNumber("42496")).toBe(false);
+    expect(validateVATNumber("4249")).toBe(false);
+    expect(validateVATNumber("424")).toBe(false);
+    expect(validateVATNumber("42")).toBe(false);
+    expect(validateVATNumber("4")).toBe(false);
+    expect(validateVATNumber("")).toBe(false);
+  });
+
+  it("should accept only numerical characters", () => {
+    expect(validateVATNumber("2379w7904")).toBe(false);
+    expect(validateVATNumber("9K7755250")).toBe(false);
   });
 
   it("works with int or string values", () => {
