85d370a458309616339d068e0a5c2ba1b547626e
Aris Giotis <apjoti@gmail.com>
2025-02-09 22:17:33 +0200

    feat(validationUtils): add phone numbers validation methods

--- src/validationUtils.ts
+++ src/validationUtils.ts
@@ -1,3 +1,4 @@
+import areaCodes from "../data/area-codes.json";
 import postalCodes from "../data/postal-codes.json";
 
 /**
@@ -100,4 +101,54 @@ export function validateVATNumber(vatNumberInput: string | number): boolean {
   const valid = calc % 10 === d9;
 
   return valid;
+}
+
+/**
+ * Validates if a given phone number is a Greek mobile phone number.
+ *
+ * @param {string} mobilePhone - The mobile phone number to validate.
+ * It may include special characters like spaces, dashes, parentheses, or dots, which will be ignored during validation.
+ *
+ * @returns {boolean} - Returns `true` if the phone number is a valid Greek mobile number; otherwise, `false`.
+ */
+export function isValidMobilePhone(mobilePhone: string): boolean {
+  const mobilePhoneRegex = RegExp(/^(\+30|0030)?69\d{8}$/);
+
+  return mobilePhoneRegex.test(mobilePhone.replace(/[\s\-()\.]/g, ""));
+}
+
+/**
+ * Validates if a given phone number is a Greek landline number.
+ *
+ * @param {string} landLinePhone - The landline phone number to validate.
+ * It may include special characters like spaces, dashes, parentheses, or dots, which will be ignored during validation.
+ *
+ * @param {boolean} [withPrefix=true] - Indicates whether the phone number should include the country prefix (`+30` or `0030`).
+ *    - If `true`, the number must start with `+30` or `0030` (optional in the regex).
+ *    - If `false`, the validation checks only for a valid area code and 7 digits.
+ *
+ * @returns {boolean} - Returns `true` if the phone number is a valid Greek landline number; otherwise, `false`.
+ */
+export function isValidLandlinePhone(landLinePhone: string, withPrefix: boolean = true): boolean {
+  const areaCodePattern = Object.values(areaCodes).join("|");
+
+  const landLinePhoneRegex = withPrefix
+    ? new RegExp(`^(\\+30|0030)?(${areaCodePattern})\\d{7}$`)
+    : new RegExp(`^(${areaCodePattern})\\d{7}$`);
+
+  const cleanedPhone = landLinePhone.replace(/[\s\-()\.]/g, "");
+
+  return landLinePhoneRegex.test(cleanedPhone);
+}
+
+/**
+ * Validates if a given phone number is either a Greek mobile or landline number.
+ *
+ * @param {string} phone - The phone number to validate.
+ * It may include special characters like spaces, dashes, parentheses, or dots, which will be ignored during validation.
+ *
+ * @returns {boolean} - Returns `true` if the phone number is a valid Greek mobile or landline number; otherwise, `false`.
+ */
+export function isValidPhone(phone: string): boolean {
+  return isValidMobilePhone(phone) || isValidLandlinePhone(phone);
 }
