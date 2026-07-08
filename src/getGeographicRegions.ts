ca96ecb9941c7b744f8c5af9dd5bda3aa80f8e7c
John Tsevdos <tsevdosjohn@gmail.com>
2026-07-08 10:55:46 +0300

    feat: tree-shakeable getGeographicRegions function

---
+++ src/getGeographicRegions.ts
@@ -0,0 +1,21 @@
+import geographicRegionsEl from "./data/geographic-regions-el.json";
+import geographicRegionsEn from "./data/geographic-regions-en.json";
+import { type Locale, type GeographicRegion } from "./types";
+
+export const geographicRegions = {
+  el: geographicRegionsEl,
+  en: geographicRegionsEn,
+} as const;
+
+type GeographicRegionOptions = { locale?: Locale };
+
+/**
+ * Returns the geographic regions in the provided locale.
+ *
+ * @param {GeographicRegionOptions} options - The options for locale.
+ *
+ * @returns {GeographicRegion[]} The geographic regions in the specified locale.
+ */
+export function getGeographicRegions({ locale = "el" }: GeographicRegionOptions = {}): GeographicRegion[] {
+  return geographicRegions[locale];
+}
